91540c7f051fc49cffd868afc592ee54783a02b1
spacegeek224 <marisusisme@gmail.com>
2015-07-21 10:30:19 -0400

    Added methods/functions

--- Calculator/Source.cpp
+++ Calculator/Source.cpp
@@ -3,16 +3,31 @@
 
 using namespace std;
 
+int getFirstNumber();
+
 int main() {
 	
-	int num1; //First number
+	/*int num1; //First number
 	int num2; //Second number
 	char operation; //Operator
 	
 	cout << "Enter first number: ";
 	cin >> num1;
-	cout << endl;
+	cout << endl;*/
+
+	getFirstNumber();
+
+	//TODO add in exception catching
 	
 
 	return 0;
 }
+
+int getFirstNumber() {
+	int num1;
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << endl;
+	return num1;
+}
+
